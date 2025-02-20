de123e31ce1878ed8174104084aef0c01830dd26
Klara923 <117095791+Klara923@users.noreply.github.com>
2025-02-20 14:59:10 +0100

    node setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 5500;
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
