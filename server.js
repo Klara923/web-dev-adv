3a5b3736bdb9dbf5543e625c8b47064c4f39d684
Klara923 <117095791+Klara923@users.noreply.github.com>
2025-02-24 21:51:08 +0100

    node docker setup

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
-const port = 5500;
+const port = 3000;
 
-app.use(express.json());
 app.use(express.static("public"));
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
